bbdf6459ac963508500e9da9f8818ea4874a4d8f
Oscar Sabet <osabet021@gmail.com>
2025-02-03 23:09:17 +0000

    updated snake game

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,246 +1,175 @@
-const videos = [
-  'assets/videos/video1.mp4',
-  'assets/videos/video2.webm',
-  'assets/videos/video3.webm',
-  'assets/videos/video4.webm'
-]
-let currentVideoIndex = 0
+// const videos = [
+//   'assets/videos/video1.mp4',
+//   'assets/videos/video2.webm',
+//   'assets/videos/video3.webm',
+//   'assets/videos/video4.webm'
+// ]
+// let currentVideoIndex = 0
 
-const heroVideo = document.getElementById('hero-video')
-const heroVideoSource = document.getElementById('hero-video-source')
+// const heroVideo = document.getElementById('hero-video')
+// const heroVideoSource = document.getElementById('hero-video-source')
 
-heroVideo.addEventListener('ended', () => {
-  currentVideoIndex = (currentVideoIndex + 1) % videos.length
-  heroVideoSource.src = videos[currentVideoIndex]
-  heroVideo.load()
-  heroVideo.play()
-})
-
-// whack-a-mole
-// document.addEventListener('DOMContentLoaded', function () {
-//   const holes = document.querySelectorAll('.hole')
-//   const startButton = document.getElementById('startButton')
-//   const endButton = document.getElementById('endButton')
-//   const scoreDisplay = document.getElementById('score')
-//   const timerDisplay = document.getElementById('timer')
-
-//   let timer
-//   let score = 0
-//   let countdown
-//   let moleInterval
-
-//   // Set the initial state to game over
-//   let gameOver = true
-
-//   function comeout () {
-//     holes.forEach(hole => {
-//       hole.classList.remove('mole')
-//       hole.removeEventListener('click', handleMoleClick)
-//     })
-
-//     let random = holes[Math.floor(Math.random() * 9)]
-
-//     random.classList.add('mole')
-//     random.addEventListener('click', handleMoleClick)
-//   }
-
-//   function handleMoleClick () {
-//     if (!gameOver) {
-//       score++
-//       scoreDisplay.textContent = `Score: ${score}`
-//     }
-//     this.classList.remove('mole')
-//   }
-
-//   function startGame () {
-//     if (!gameOver) {
-//       // Prevent starting the game
-//       // again if it's already in progress
-//       return
-//     }
-
-//     gameOver = false
-//     score = 0
-//     scoreDisplay.textContent = `Score: ${score}`
-//     timer = 60
-//     timerDisplay.textContent = `Time: ${timer}s`
-
-//     startButton.disabled = true
-//     endButton.disabled = false
-
-//     countdown = setInterval(() => {
-//       timer--
-//       timerDisplay.textContent = `Time: ${timer}s`
-
-//       if (timer <= 0) {
-//         clearInterval(countdown)
-//         gameOver = true
-//         alert(`Game Over!\nYour final score: ${score}`)
-//         startButton.disabled = false
-//         endButton.disabled = true
-//       }
-//     }, 1000)
-
-//     moleInterval = setInterval(() => {
-//       if (!gameOver) comeout()
-//     }, 1000)
-
-//     console.log('Game started')
-//   }
-
-//   function endGame () {
-//     clearInterval(countdown)
-//     clearInterval(moleInterval)
-//     gameOver = true
-//     alert(`Game Ended!\nYour final score: ${score}`)
-//     score = 0
-//     timer = 60
-//     scoreDisplay.textContent = `Score: ${score}`
-//     timerDisplay.textContent = `Time: ${timer}s`
-//     startButton.disabled = false
-//     endButton.disabled = true
-//   }
-
-//   startButton.addEventListener('click', startGame)
-//   endButton.addEventListener('click', endGame)
+// heroVideo.addEventListener('ended', () => {
+//   currentVideoIndex = (currentVideoIndex + 1) % videos.length
+//   heroVideoSource.src = videos[currentVideoIndex]
+//   heroVideo.load()
+//   heroVideo.play()
 // })
 
 // SNAKE GAME
 // function snakeGame () {
-const canvas = document.getElementById('snake')
+const canvas = document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2d')
+const scoreElement = document.getElementById('snake-score')
+const timerElement = document.getElementById('snake-time')
 
+// const parentContainer = document.getElementById('snake-container')
+// canvas.width = parentContainer.clientWidth
+// canvas.height = parentContainer.clientHeight
+
+console.log('canvas.width', canvas.width)
 const frameRate = 1000 / 10
 
 let userInput = 'ArrowUp'
-
+let lastInput = 'ArrowUp'
 let frameCounter = 0
 
-const gameWidth = 1000
-const gameHeight = 1000
+// const gameWidth = 600
+// const gameHeight = 600
+
+const gameWidth = 400
+const gameHeight = 400
 
 canvas.width = gameWidth
 canvas.height = gameHeight
+canvas.style.width = '100%'
+canvas.style.height = '100%'
 
 const sSize = 10
-const snakeStartingLength = 3
-let snake = []
-for (let i = 0; i < snakeStartingLength; i++) {
-  snake.push([canvas.width / 2 - sSize, canvas.height / 2 - sSize])
-  console.log('snake', snake)
-}
+const snakeStartingLength = 1
 
+let snake = []
+
+let fruitImage = new Image()
+fruitImage.src = 'assets/snake/apple.png'
+const fruitSize = 40
 fruit = [
   Math.floor(Math.random() * 30) * 10,
   Math.floor(Math.random() * 30) * 10
 ]
 
-const fruitSize = 10
-
-// let userInput = 'up'
+let background = new Image()
+background.src = 'assets/snake/grass.png'
 
 const scoreText = 'Score: '
 const timerText = 'Time: '
 let score = 0
 let timer = 0
 
-let gameOver = false
+const introText = 'Welcome to Snake'
+const startText = 'Press Enter to Start'
+const gameOverText = 'Game Over'
 
-document.addEventListener('keydown', e => {
-  console.log(e.key)
-  console.log('userddddInput', userInput)
+let gameState = 'intro'
 
-  switch (e.key) {
-    case 'ArrowUp' || 'w':
-      userInput = 'ArrowUp'
-      break
-    case 'ArrowDown' || 's':
-      userInput = 'ArrowDown'
-      break
-    case 'ArrowLeft' || 'a':
-      userInput = 'ArrowLeft'
-      break
-    case 'ArrowRight' || 'd':
-      userInput = 'ArrowRight'
-      break
-    // default:
-    //   userInput = userInput
-  }
-})
+const eatSound = new Audio('assets/snake/sounds/EatSound.ogg')
 
-// document.addEventListener('keydown', e => {
-//   console.log(e.key)
-//   switch (e.key) {
-//     case 'ArrowUp' || 'w':
-//       userInput = 'ArrowUp'
-//     case 'ArrowDown' || 's':
-//       userInput = 'ArrowDown'
-//     case 'ArrowLeft' || 'a':
-//       userInput = 'ArrowLeft'
-//     case 'ArrowRight' || 'd':
-//       userInput = 'ArrowRight'
-//     // default:
-//     //   userInput = userInput
-//   }
-// })
-
-function draw () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  ctx.fillStyle = 'green'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = 'red'
-  ctx.fillRect(fruit[0], fruit[1], 10, 10)
-
-  ctx.fillStyle = 'black'
-  if (snake.length) {
-    for (let i = 0; i < snake.length; i++) {
-      ctx.fillStyle = 'black'
-      ctx.fillRect(snake[i][0], snake[i][1], sSize, sSize)
-    }
-  }
-  ctx.fillStyle = 'black'
-  ctx.fillText(`${scoreText}${score}`, 10, 10)
-  ctx.fillText(`${timerText}${timer}`, 10, 20)
-}
-
-gameOver = false
-score = 0
-timer = 0
-// snake = [[0, 0]]
-fruit = [
-  Math.floor(Math.random() * 30) * 10,
-  Math.floor(Math.random() * 30) * 10
-]
+// Load and loop background music
+const backgroundMusic = new Audio('assets/snake/sounds/level-1-bg.wav')
+backgroundMusic.loop = true
+backgroundMusic.volume = 0.2 // Set volume to 20%
 
 setInterval(() => {
   timer++
 }, 1000)
 
-// console.log('Game started')
+let startButton = document.getElementById('snakeButton')
+startButton.addEventListener('click', e => {
+  gameState = 'playing'
+})
 
-// function getInput (input) {
-//   window.addEventListener('keydown', e => {
-//     switch (e.key) {
-//       case 'ArrowUp':
-//         return 'up'
-//       case 'ArrowDown':
-//         return 'down'
-//       case 'ArrowLeft':
-//         return 'left'
-//       case 'ArrowRight':
-//         return 'right'
-//       default:
-//         return input
-//     }
-//   })
-// }
+document.addEventListener('keydown', e => {
+  // console.log(e.key)
+  // console.log('userddddInput', userInput)
 
+  switch (e.key) {
+    case 'ArrowUp':
+    case 'w':
+      if (lastInput !== 'ArrowDown') userInput = 'ArrowUp'
+      break
+    case 'ArrowDown':
+    case 's':
+      if (lastInput !== 'ArrowUp') userInput = 'ArrowDown'
+      break
+    case 'ArrowLeft':
+    case 'a':
+      if (lastInput !== 'ArrowRight') userInput = 'ArrowLeft'
+      break
+    case 'ArrowRight':
+    case 'd':
+      if (lastInput !== 'ArrowLeft') userInput = 'ArrowRight'
+      break
+    case 'Enter':
+      gameState = 'playing'
+      break
+  }
+})
+/**
+ * Initialize the game & reset variables for new game
+ */
+function initGame () {
+  score.innerText = '0'
+  timer.innerText = '0'
+  lastInput = 'ArrowUp'
+  userInput = 'ArrowUp'
+
+  snake = []
+  for (let i = 0; i < snakeStartingLength; i++) {
+    snake.push([canvas.width / 2 - sSize, canvas.height / 2 - sSize])
+  }
+
+  fruit = [
+    Math.floor(Math.random() * (gameWidth / fruitSize)) * fruitSize,
+    Math.floor(Math.random() * (gameHeight / fruitSize)) * fruitSize
+  ]
+}
+/**
+ * Draw the game to the screen
+ */
+function draw () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+  ctx.drawImage(fruitImage, fruit[0], fruit[1], fruitSize, fruitSize)
+
+  ctx.fillStyle = 'black'
+  if (snake.length) {
+    for (let i = 0; i < snake.length; i++) {
+      ctx.fillStyle = 'darkgreen'
+      ctx.strokeStyle = 'yellow'
+      ctx.fillRect(snake[i][0], snake[i][1], sSize, sSize)
+      ctx.strokeRect(snake[i][0], snake[i][1], sSize, sSize)
+      // ctx.(snakePart.x, snakePart.y, 10, 10);}
+    }
+  }
+  ctx.font = '10px Arial'
+  ctx.fillStyle = 'yellow'
+  ctx.fillText(`${scoreText}${score}`, 10, 10)
+  ctx.fillText(`${timerText}${timer}`, 10, 20)
+  timerElement.innerText = timer
+  scoreElement.innerText = score
+}
+
+/**
+ * Moves snake and checks collision
+ * @param {*} input User input
+ * @param {*} sSize Size of snake
+ * @returns none
+ */
 function moveSnake (input, sSize) {
-  // console.log(input)
-
   let newHead = [snake[snake.length - 1][0], snake[snake.length - 1][1]]
-  console.log('input', input)
   switch (input) {
     case 'ArrowUp':
       newHead = [snake[snake.length - 1][0], snake[snake.length - 1][1] - sSize]
@@ -255,64 +184,89 @@ function moveSnake (input, sSize) {
       newHead = [snake[snake.length - 1][0] + sSize, snake[snake.length - 1][1]]
       break
   }
+  lastInput = input
+
+  for (let i = 0; i < snake.length; i++) {
+    if (newHead[0] === snake[i][0] && newHead[1] === snake[i][1]) {
+      gameState = 'gameover'
+      return
+    }
+  }
   snake.push(newHead)
 
   let sLength = snake.length - 1
   if (snake.length) {
-    if (snake[sLength][0] === fruit[0] && snake[sLength][1] === fruit[1]) {
+    const head = snake[sLength]
+    if (
+      head[0] < fruit[0] + fruitSize &&
+      head[0] + sSize > fruit[0] &&
+      head[1] < fruit[1] + fruitSize &&
+      head[1] + sSize > fruit[1]
+    ) {
+      eatSound.play()
       score++
       fruit = [
-        Math.floor(Math.random() * 30) * 10,
-        Math.floor(Math.random() * 30) * 10
+        Math.floor(Math.random() * (gameWidth / fruitSize)) * fruitSize,
+        Math.floor(Math.random() * (gameHeight / fruitSize)) * fruitSize
       ]
     } else {
-      // snake.pop()
       snake.shift()
     }
-    sLength = snake.length - 1
+  }
 
-    console.log(snake[sLength][0], snake[sLength][1])
-
-    if (
-      snake[sLength][0] < 0 ||
-      snake[sLength][0] >= canvas.width + sSize ||
-      snake[sLength][1] < 0 ||
-      snake[sLength][1] >= canvas.height - sSize
-    ) {
-      gameOver = true
-    }
+  sLength = snake.length - 1
+  if (
+    snake[sLength][0] < 0 ||
+    snake[sLength][0] >= canvas.width + sSize ||
+    snake[sLength][1] < 0 ||
+    snake[sLength][1] >= canvas.height - sSize
+  ) {
+    gameState = 'gameover'
   }
 }
 
 function gameEnd () {
-  console.log('Game Over!')
+  initGame()
+  gameState = 'intro'
 }
+
 function gameIntro () {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  ctx.fillStyle = 'green'
+  ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  ctx.font = '30px Arial'
+  ctx.fillStyle = 'white'
+  ctx.fillText(
+    introText,
+    canvas.width / 2 - ctx.measureText(introText).width / 2,
+    canvas.height / 2 - 30
+  )
   ctx.fillStyle = 'red'
-  ctx.fillRect(fruit[0], fruit[1], 10, 10)
-
-  ctx.fillText(`${scoreText}${score}`, 10, 10)
-  ctx.fillText(`${timerText}${timer}`, 10, 20)
+  ctx.fillText(
+    startText,
+    canvas.width / 2 - ctx.measureText(introText).width / 2,
+    canvas.height / 2 + 30
+  )
 }
+
 function startGame () {
-  // frameCounter++
-  // console.log('frameCounter', frameCounter)
-  // userInput = getInput(userInput)
-  moveSnake(userInput, sSize)
-  draw()
-  if (gameOver) {
-    gameEnd()
+  if (gameState === 'intro') {
     gameIntro()
-    return
-  } else {
-    setTimeout(() => {
-      startGame()
-    }, frameRate)
-  }
-}
+  } else if (gameState === 'playing') {
+    moveSnake(userInput, sSize)
+    draw()
 
+    backgroundMusic.play()
+  } else if (gameState === 'gameover') {
+    backgroundMusic.pause()
+    gameEnd()
+  }
+
+  setTimeout(() => {
+    startGame()
+  }, frameRate)
+}
+initGame()
 startGame()
