8cf64a873161313902123cc9741ad5e685f4cd84
ShockSchool <er0nleyy@students.codeinstitute.net>
2025-02-03 22:01:02 +0000

    Questions in an array, second question
    Questions are still hard coded but now in array form, added a second question that it will change to if the first is right, score goes up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-const question1 = {number:1, question:"The name of which arthropod means one hundred feet in Latin?", answer:"centipede", lastLetters:"ede"};
-const picture1 = {number:1, url:"assets/images/Eden_project.jpg", answer:"eden project", firstLetters:"ede", lettersToCut: 3};
+const questions = [];
+const pictures = [];
+questions[0] = {number:1, question:"The name of which arthropod means one hundred feet in Latin?", answer:"centipede", lastLetters:"ede"};
+pictures[0] = {number:1, url:"assets/images/Eden_project.jpg", answer:"eden project", firstLetters:"ede", lettersToCut:3};
+questions[1] = {number:2, question:"Which game in <em>The Legend of Zelda</em> series released on the Super NES saw Link travel between the Light World and the Dark World?", answer:"a link to the past", lastLetters: "past"};
+pictures[1] = {number:2, url:"assets/images/picture2.png", answer:"pasty", firstLetters:"past", lettersToCut:4};
+// const questions = ["question1", "question2"];
+// const pictures = ["picture1", "picture2"];
+questionNumber = 0;
 
 // document.addEventListener("DOMContentLoaded", function() {
 // 	let submitAnswer = document.getElementById("combo-submit");
@@ -15,10 +22,10 @@ const picture1 = {number:1, url:"assets/images/Eden_project.jpg", answer:"eden p
 document.addEventListener("DOMContentLoaded", runGame);
 
 function runGame() {
-	document.getElementById("question").textContent = question1.question;
-	document.getElementById("picture").src = picture1.url;
+	document.getElementById("question").textContent = questions[questionNumber].question;
+	document.getElementById("picture").src = pictures[questionNumber].url;
 	document.getElementById("picture").style.maxHeight = "300px";
-	document.getElementById("answer-test").textContent = getAnswer();
+	// document.getElementById("answer-test").textContent = getAnswer();
 	document.getElementById("combo-submit").onclick = checkAnswer;
 	let textInput = document.getElementById("answer-box");
 	textInput.addEventListener("keypress", function(event) {
@@ -31,8 +38,8 @@ function runGame() {
 }
 
 function getAnswer() {
-	let suffix = picture1.answer.slice(picture1.lettersToCut);
-	let combination = question1.answer.concat(suffix);
+	let suffix = pictures[questionNumber].answer.slice(pictures[questionNumber].lettersToCut);
+	let combination = questions[questionNumber].answer.concat(suffix);
 	return combination;
 }
 
@@ -42,11 +49,16 @@ function checkAnswer() {
 	let isCorrect = userAnswer === calculatedAnswer;
 	if (isCorrect) {
 		alert("Hey! You got it right! :D");
-		//incrementScore();
+		incrementScore();
+		++questionNumber;
+		runGame();
 	} else {
 		alert(`Your answer of ${userAnswer} was wrong, the correct answer is ${calculatedAnswer}!`)
 		//incrementWrongAnswer();
 	}
 }
 
-// runGame();
+function incrementScore() {
+	let oldScore = parseInt(document.getElementById("combo-score").innerText);
+	document.getElementById("combo-score").innerText = ++oldScore
+}
