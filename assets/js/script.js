e44902c8036047d35dce6e96bed282066cef36b3
ShockSchool <er0nleyy@students.codeinstitute.net>
2025-02-04 10:38:29 +0000

    Game loops around
    Also deleted old comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,31 @@
 const questions = [];
 const pictures = [];
-questions[0] = {number:1, question:"The name of which arthropod means one hundred feet in Latin?", answer:"centipede", lastLetters:"ede"};
-pictures[0] = {number:1, url:"assets/images/Eden_project.jpg", answer:"eden project", firstLetters:"ede", lettersToCut:3};
-questions[1] = {number:2, question:"Which game in <em>The Legend of Zelda</em> series released on the Super NES saw Link travel between the Light World and the Dark World?", answer:"a link to the past", lastLetters: "past"};
-pictures[1] = {number:2, url:"assets/images/picture2.png", answer:"pasty", firstLetters:"past", lettersToCut:4};
-// const questions = ["question1", "question2"];
-// const pictures = ["picture1", "picture2"];
+questions[0] = {question:"The name of which arthropod means one hundred feet in Latin?", answer:"centipede", lastLetters:"ede"};
+pictures[0] = {url:"assets/images/Eden_project.jpg", answer:"eden project", firstLetters:"ede", lettersToCut:3};
+questions[1] = {question:"Which game in The Legend of Zelda series released on the Super NES saw Link travel between the Light World and the Dark World?", answer:"a link to the past", lastLetters: "past"};
+pictures[1] = {url:"assets/images/picture2.png", answer:"pasty", firstLetters:"past", lettersToCut:4};
 questionNumber = 0;
+questionsTotal = 2;
 
-// document.addEventListener("DOMContentLoaded", function() {
-// 	let submitAnswer = document.getElementById("combo-submit");
-// 	submitAnswer.addEventListener("click", function() {
-// 		if (this.getAttribute("data-type") === "submit") {
-// 			checkAnswer();
-// 		} else {
-// 			runGame();
-// 		}
-// 	})
-// 	runGame();
-// })
-document.addEventListener("DOMContentLoaded", runGame);
+document.addEventListener("DOMContentLoaded", init);
 
-function runGame() {
-	document.getElementById("question").textContent = questions[questionNumber].question;
-	document.getElementById("picture").src = pictures[questionNumber].url;
-	document.getElementById("picture").style.maxHeight = "300px";
-	// document.getElementById("answer-test").textContent = getAnswer();
-	document.getElementById("combo-submit").onclick = checkAnswer;
+function init() {
 	let textInput = document.getElementById("answer-box");
 	textInput.addEventListener("keypress", function(event) {
 		if (event.key === "Enter") {
 			checkAnswer();
 		}
 	});
-	// let submitAnswer = document.getElementById("combo-submit");
-	// submitAnswer.addEventListener("click", checkAnswer);
+	runGame();
+}
+
+function runGame() {
+	document.getElementById("question").textContent = questions[questionNumber].question;
+	document.getElementById("picture").src = pictures[questionNumber].url;
+	document.getElementById("picture").style.maxHeight = "300px";
+	document.getElementById("answer-box").value = "";
+	// document.getElementById("answer-test").textContent = getAnswer();
+	document.getElementById("combo-submit").onclick = checkAnswer;
 }
 
 function getAnswer() {
@@ -51,10 +42,12 @@ function checkAnswer() {
 		alert("Hey! You got it right! :D");
 		incrementScore();
 		++questionNumber;
+		if (questionNumber == questionsTotal) {
+			questionNumber = 0;
+		}
 		runGame();
 	} else {
 		alert(`Your answer of ${userAnswer} was wrong, the correct answer is ${calculatedAnswer}!`)
-		//incrementWrongAnswer();
 	}
 }
 
